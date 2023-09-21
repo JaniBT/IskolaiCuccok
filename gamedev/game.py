337f2492d4ffdb6d1c25170ca54c56c5a4c60ef6
JaniBT <janoscsordas072@gmail.com>
2023-09-21 12:10:54 +0200

    Gamedev folyt

--- gamedev/game.py
+++ gamedev/game.py
@@ -1,22 +1,9 @@
 from ursina import *
-
-class Player(Entity):
-    def input(self, key):
-        if key == 'w':
-            self.position += self.forward
-        if key == 'd':
-            self.animate('rotation_y', self.rotation_y + 90, duration=.1)
-
-        if key == 'a':
-            self.animate('rotation_y', self.rotation_y - 90, duration=.1)
-        if key == 's':
-            self.position -= self.forward
-
+import random
 
 
 app = Ursina()
 
-Entity = Sprite('cube')
-Player()
+p1 = Entity(model='cube', texture='')
 
 app.run()
