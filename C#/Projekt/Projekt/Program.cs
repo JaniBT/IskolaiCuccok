cfc6effc37933508d6d549bd1870b89ba3c1bed1
JaniBT <janoscsordas072@gmail.com>
2023-10-25 20:48:01 +0200

    asd

---
+++ C#/Projekt/Projekt/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+
+namespace Projekt
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Adj meg egy számot: ");
+            int szam = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Add meg, hogy milyen műveletet akarsz elvégezni (/*-+): ");
+            string muvelet = Console.ReadLine();
+            Console.WriteLine("Adj meg egy másik számot: ");
+            int szamketto = Convert.ToInt32(Console.ReadLine());
+            if (muvelet == "/")
+            {
+                int oszt = szam / szamketto;
+                Console.WriteLine("Az eredmény: ", oszt);
+            }
+            else if (muvelet == "*")
+            {
+                int szoroz = szam * szamketto;
+                Console.WriteLine("Az eredmény: ", szoroz);
+            }
+            else if (muvelet == "-")
+            {
+                int kivon = szam - szamketto;
+                Console.WriteLine("Az eredmény: ", kivon);
+            }
+            else if (muvelet == "+")
+            {
+                int osszead = szam + szamketto;
+                Console.WriteLine("Az eredmény: ", osszead);
+            }
+            else
+            {
+                Console.WriteLine("Csak megadott műveleti jeleket adhatsz meg!");
+            }
+        }
+    }
+}
