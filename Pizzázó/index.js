f3084760a0c6f4d152b88e777523b0704659481b
JaniBT <janoscsordas072@gmail.com>
2023-11-12 16:00:55 +0100

    Upgrade people, upgrade

--- Pizzázó/index.js
+++
@@ -1,9 +0,0 @@
-function megjelenitKod() {
-    // Az elem kódjának lekérése
-    var valasztottElem = document.getElementById("elemValaszto");
-    var elemKodja = valasztottElem.value;
-
-    // Az elem kódjának megjelenítése a HTML oldalon
-    var kodMegjelenito = document.getElementById("kodMegjelenito");
-    kodMegjelenito.innerHTML = "A kiválasztott elem kódja: " + elemKodja;
-}
